8fa5e6e151c6bc6d4dafde89461f7e0c1b5bae08
mattkantor <matthewkantor@gmail.com>
2016-02-23 13:46:19 -0500

    add forms to the footer for collection

--- wp-content/themes/crtc/_newsletter.php
+++ wp-content/themes/crtc/_newsletter.php
@@ -1,8 +1,8 @@
 	<div class="row">
 		<div class="col-sm-12 text-center">
 			<h2>Sign up for our newsletter</h2>
-			<form>
-				<input type="text" placeholder="email address"/><input type="submit">
+			<form target="_blank" method='get' accept-charset='UTF-8' name='oi_form' action="https://fb-subscribe-form.com/oi/1132/99b504a1b001b8fda57eacfd9e4edd59">
+				<input type="text" name="email" placeholder="email address"/><input type="submit">
 			</form>
 		</div>
 

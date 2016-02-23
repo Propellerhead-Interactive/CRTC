6f0b0331c84c5edccecdeaed487bd79f2af28722
mattkantor <matthewkantor@gmail.com>
2016-02-23 13:59:03 -0500

    add prod form code

--- wp-content/themes/crtc/_newsletter.php
+++ wp-content/themes/crtc/_newsletter.php
@@ -1,8 +1,11 @@
 	<div class="row">
 		<div class="col-sm-12 text-center">
 			<h2>Sign up for our newsletter</h2>
-			<form target="_blank" method='get' accept-charset='UTF-8' name='oi_form' action="https://fb-subscribe-form.com/oi/1132/99b504a1b001b8fda57eacfd9e4edd59">
-				<input type="text" name="email" placeholder="email address"/><input type="submit">
+			<form target="_blank" method='get' accept-charset='UTF-8' name='oi_form' action="https://fb-subscribe-form.com/oi/1132/42034d257db5ed20ecaf7c9a6994b178">
+				<input type="text" name="email" placeholder="email address"/>
+				<input type="hidden" name="mailingList" value="yes"/>
+				
+				<input type="submit">
 			</form>
 		</div>
 

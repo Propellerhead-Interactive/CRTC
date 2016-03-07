8afb81133011d846d1f958f511527909fe96b806
mattkantor <matthewkantor@gmail.com>
2016-03-06 21:02:03 -0500

    plugin

--- wp-content/themes/crtc/_newsletter.php
+++ wp-content/themes/crtc/_newsletter.php
@@ -1,7 +1,12 @@
 <h1><?php  _e("SIGN UP FOR <br /> OUR MAILING LIST");?></h1>
 <div class="row">
   <div class="col-sm-4 col-sm-offset-4">
-		<form style="margin-bottom: 40px" target="_blank" method='get' accept-charset='UTF-8' name='oi_form' action="https://fb-subscribe-form.com/oi/1132/42034d257db5ed20ecaf7c9a6994b178">
+		<form style="margin-bottom: 40px" target="_blank" method='get' accept-charset='UTF-8' name='oi_form' action="<?php if(ICL_LANGUAGE_CODE=='en'){echo "https://fb-subscribe-form.com/oi/1132/42034d257db5ed20ecaf7c9a6994b178";
+      }else{
+      echo "https://fb-subscribe-form.com/oi/1132/42034d257db5ed207f69b6e7512f50c3";
+      }
+      
+      ?>">
 			<input type="hidden" name="mailingList" value="yes"/>
 			<div class="input-group">
 			  <input type="text" class="form-control __placeholder-pink" name="email" placeholder="<?php  _e("EMAIL ADDRESS");?>">
@@ -12,3 +17,4 @@
 		</form>
 	</div>
 </div>
+

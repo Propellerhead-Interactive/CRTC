0b59ad45a5856d0e3e2dd181ae60e7e63d2beb1a
mattkantor <matthewkantor@gmail.com>
2016-03-12 04:17:03 -0500

    update widget

--- wp-content/themes/crtc/_social_block.php
+++ wp-content/themes/crtc/_social_block.php
@@ -4,6 +4,7 @@
   <?php echo do_shortcode(get_field("instagram"));?>
 </div>
 <div class="col-sm-3">
-	<a class="twitter-timeline" href="https://twitter.com/hashtag/<?php echo do_shortcode(get_field("twitter_hashtag"));?>" data-widget-id="658720264046252036"><?php _e("Tweet #discoverability");?></a>
+	<a class="twitter-timeline" href="https://twitter.com/hashtag/<?php echo do_shortcode(get_field("twitter_hashtag"));?>" 
+    data-widget-id="<?php if(ICL_LANGUAGE_CODE=="en"){echo "658720264046252036"}else{echo "659809250638585857"}?>"><?php _e("Tweet #discoverability");?></a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>

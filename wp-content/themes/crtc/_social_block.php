7621e25a099413e5484d0b950821ea14312c6f97
mattkantor <matthewkantor@gmail.com>
2016-03-12 04:18:06 -0500

    fix bug

--- wp-content/themes/crtc/_social_block.php
+++ wp-content/themes/crtc/_social_block.php
@@ -5,6 +5,6 @@
 </div>
 <div class="col-sm-3">
 	<a class="twitter-timeline" href="https://twitter.com/hashtag/<?php echo do_shortcode(get_field("twitter_hashtag"));?>" 
-    data-widget-id="<?php if(ICL_LANGUAGE_CODE=="en"){echo "658720264046252036"}else{echo "659809250638585857"}?>"><?php _e("Tweet #discoverability");?></a>
+    data-widget-id="<?php if(ICL_LANGUAGE_CODE=="en"){echo "658720264046252036";}else{echo "659809250638585857";}?>"><?php _e("Tweet #discoverability");?></a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>

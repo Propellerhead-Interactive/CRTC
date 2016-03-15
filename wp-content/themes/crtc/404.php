f18a03a074f5c28579646074d7c6b449fddf2839
mattkantor <matthewkantor@gmail.com>
2016-03-15 17:32:18 -0400

    404

--- wp-content/themes/crtc/404.php
+++ wp-content/themes/crtc/404.php
@@ -11,11 +11,12 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
+					<h1 class="page-title"><?php _e("404 Error");?></h1>
+            <h3><?php _e( 'Unfortunately the page you were looking for does not exist', 'twentysixteen' ); ?></h3>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
+					<p><?php _e( 'Don\'t worry! You can always: <A href="http://discoverability.ca">go to homepage</a>', 'twentysixteen' ); ?></p>
 
 					<?php get_search_form(); ?>
 				</div><!-- .page-content -->

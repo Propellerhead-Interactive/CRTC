25667eca20c5828a028ffcfb630c51051f0673cd
Phil Moreira <philm@propellerhead.ca>
2016-03-15 16:53:39 -0400

    qa fixes

--- wp-content/themes/crtc/header.php
+++ wp-content/themes/crtc/header.php
@@ -15,6 +15,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/favicon.ico">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
